15f7a1fc4b0c11b6a1a94e9b04a37369302cba63
Caio SBA <caiosba@gmail.com>
2015-11-16 15:03:39 -0300

    Adding webpack configuration

---
+++ app/index.js
@@ -0,0 +1,7 @@
+require('./main.css');
+var component = require('./component');
+var app = document.createElement('div');
+
+document.body.appendChild(app);
+
+app.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+module.exports = {
+  entry: APP_PATH,
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: APP_PATH
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    host: process.env.HOST,
+    port: 8888
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'Bridge'
+    })
+  ]
+};
