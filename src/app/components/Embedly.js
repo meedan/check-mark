da2c960ea417b199d42977a38f8f7ae5d20eee72
Caio SBA <caiosba@gmail.com>
2015-12-01 11:20:04 -0300

    Ticket #4351: Use Twitter embed directly instead of Embed.ly if domain is Twitter

--- src/app/components/Embedly.js
+++ src/app/components/Embedly.js
@@ -2,25 +2,31 @@ import React, { Component, PropTypes } from 'react';
 import scriptjs from 'scriptjs';
 
 class Embedly extends Component {
+  domain() {
+    return this.props.url.split('/')[2];
+  }
+
   componentDidMount() {
-    scriptjs('https://cdn.embedly.com/widgets/platform.js', function() {
-      var element = document.getElementById('embedly');
-      window.embedly('card', element);
-    });
+    if (this.domain() === 'twitter.com') {
+      scriptjs('https://platform.twitter.com/widgets.js', function() {
+        window.twttr.widgets.load();
+      });
+    }
+    else {
+      scriptjs('https://cdn.embedly.com/widgets/platform.js', function() {
+        var element = document.getElementById('embedly');
+        window.embedly('card', element);
+      });
+    }
   }
 
   render() {
-    return (
-      <div className="embedly">
-        <a
-          data-card-controls="0"
-          data-card-width="100%"
-          className="embedly-card"
-          id="embedly"
-          href={this.props.url}>
-        </a>
-      </div>
-    );
+    if (this.domain() === 'twitter.com') {
+      return (<div className="embed"><blockquote className="twitter-tweet" lang="pt"><a href={this.props.url}></a></blockquote></div>);
+    }
+    else {
+      return (<div className="embed"><a data-card-controls="0" data-card-width="100%" id="embedly" href={this.props.url}></a></div>);
+    }
   }
 }
 
