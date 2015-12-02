b7292b1d4bedd9243dcd9181ec92c976b65e6396
Caio SBA <caiosba@gmail.com>
2015-12-02 10:03:09 -0300

    Ticket #4351: Implementing event callbacks for translation fields

--- src/app/components/SaveTranslation.js
+++ src/app/components/SaveTranslation.js
@@ -3,8 +3,23 @@ import SelectProject from './SelectProject';
 import Embedly from './Embedly';
 import Select from 'react-select';
 import BackBar from './BackBar';
+import util from 'util';
 
 class SaveTranslation extends Component {
+  onTranslationFocus() {
+    var field = React.findDOMNode(this.translation)
+    field.value = "";
+    field.style.backgroundColor = '#F4F4F4';
+  }
+
+  onTranslationBlur() {
+    React.findDOMNode(this.translation).style.backgroundColor = '#FFF';
+  }
+
+  onAnnotationFocus() {
+    React.findDOMNode(this.annotation).value = "";
+  }
+
   render() {
     const { loginTwitter, loginFacebook, goBack, savePost, submitPost, saveTranslation, submitTranslation, state } = this.props;
     return (
@@ -18,13 +33,24 @@ class SaveTranslation extends Component {
               <form onSubmit={submitTranslation.bind(this)}>
                 
                 <label for="translation">Translation</label>
-                <textarea name="translation-text" id="translation">Enter your translation here</textarea>
+
+                <textarea name="translation"
+                          id="translation"
+                          onFocus={this.onTranslationFocus.bind(this)} 
+                          onBlur={this.onTranslationBlur.bind(this)} 
+                          ref={(ref) => this.translation = ref}>Enter your translation here</textarea>
+
                 <label for="annotation">Annotation</label>
-                <textarea name="annotation-text" id="annotation">Enter your annotation here</textarea>
+
+                <textarea name="annotation" 
+                          id="annotation"
+                          onFocus={this.onAnnotationFocus.bind(this)} 
+                          ref={(ref) => this.annotation = ref}>Enter your annotation here</textarea>
                 
                 <div className="select-project">
                   <SelectProject projects={state.extension.projects} />
                 </div>
+
                 <div className="select-language">
                   <label>Language</label>
                   <Select name="language" value="" options={state.extension.languages} className="dropdown" />
