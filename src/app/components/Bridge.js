f8248687c540ea4cea80ba98314a8d897e894059
Caio SBA <caiosba@gmail.com>
2016-01-06 02:29:40 -0300

    Ticket #4471: Take user to menu if he is already logged in through the same provider as the current post

--- src/app/components/Bridge.js
+++ src/app/components/Bridge.js
@@ -1,4 +1,6 @@
 import React, { Component, PropTypes } from 'react';
+import superagent from 'superagent';
+import config from '../config/config.js';
 import Login from './Login';
 import Menu from './Menu';
 import Message from './Message';
@@ -7,6 +9,19 @@ import SaveTranslation from './SaveTranslation';
 import ListTranslations from './ListTranslations';
 
 class Bridge extends Component {
+  currentProvider(state) {
+    var domain = state.extension.url.split('/')[2];
+    if (/twitter\.com$/.test(domain)) {
+      return 'twitter';
+    }
+    else if (/facebook\.com$/.test(domain)) {
+      return 'facebook';
+    }
+    else {
+      return null;
+    }
+  }
+
   pageSupported(state) {
     if (!/(facebook|twitter)\.com$/.test(state.extension.url.split('/')[2])) {
       state.bridge.message = '<h1>Oops - right now, Bridge only works with posts from Twitter or Facebook. Try again?</h1>';
@@ -15,17 +30,58 @@ class Bridge extends Component {
     return true;
   }
 
+  loggedIn(state) {
+    var that     = this,
+        provider = that.currentProvider(state),
+        fail     = function() {
+          state.bridge.view = 'login';
+          that.forceUpdate();
+        },
+        success  = function() {
+          state.bridge.view = 'menu';
+          that.forceUpdate();
+        }
+
+    if (provider === null) {
+      fail();
+    }
+
+    else if (state && state.bridge && state.bridge.session && state.bridge.session.provider === provider) {
+      success();
+    }
+
+    else {
+      superagent.get(config.bridgeApiBase + '/api/users/' + provider + '_info')
+      .end(function(err, response) {
+        if (err) {
+          fail();
+        }
+        else if (response.text === 'null') {
+          fail();
+        }
+        else {
+          state.bridge.session = JSON.parse(response.text);
+          success();
+        }
+      });
+    }
+  }
+
   render() {
     const { loginTwitter, loginFacebook, goBack, savePost, submitPost, saveTranslation, submitTranslation, myTranslations, state } = this.props;
-    let view = ((state && state.bridge && state.bridge.view) ? state.bridge.view : 'login');
+
+    let view = ((state && state.bridge && state.bridge.view) ? state.bridge.view : 'home');
 
     switch (view) {
+      case 'home':
+        this.loggedIn(state);
+        return null;
+      case 'message':
+        return (<Message {...this.props} />);
       case 'login':
         return (<Login {...this.props} />);
       case 'menu':
         return (<Menu {...this.props} />);
-      case 'message':
-        return (<Message {...this.props} />);
       case 'save_post':
         if (this.pageSupported(state)) {
           return (<SavePost {...this.props} />);
