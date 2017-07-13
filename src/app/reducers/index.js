1f9c364e02e3a019c0ac3cbc4f75ddc1af9b651a
Caio SBA <caiosba@gmail.com>
2017-07-13 14:46:28 -0300

    Implementing a new version from scratch

--- src/app/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-import bridge from './bridge';
-import extension from './extension';
-
-export default combineReducers({ bridge, extension });
--- src/plugins/babelRelayPlugin.js
+++
@@ -1,3 +0,0 @@
-var getbabelRelayPlugin = require('babel-relay-plugin');
-var schema = require('./../../data/relay.json');
-module.exports = getbabelRelayPlugin(schema.data);
