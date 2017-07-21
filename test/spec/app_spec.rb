047819ebb2ce84fb7b8d53d6215f107a920e74da
Caio SBA <caiosba@gmail.com>
2017-07-20 21:43:10 -0300

    Ticket #5956: Completed automated tests for Chrome and Firefox

--- test/spec/app_spec.rb
+++ test/spec/app_spec.rb
@@ -3,12 +3,35 @@ require 'yaml'
 require 'net/http'
 require 'json'
 
-describe 'app' do
+shared_examples 'tests' do
+  it 'should open extension' do
+    open_extension
+    message = get_element('#app h2')
+    expect(message.text.include?('Add to Check')).to be(true)
+  end
 
-  @driver = @config = @prefs = nil
+  it 'should localize' do
+    open_browser 'pt'
+    open_extension
+    message = get_element('#app h2')
+    expect(message.text.include?('Adicionar ao Check')).to be(true)
+    open_browser 'en'
+  end
+end
+
+describe 'app' do
+  @driver = @config = nil
+
+  before :all do
+    @config = YAML.load_file('config.yml')
+  end
 
   before :each do
-    @config = YAML.load_file('config.yml')
+    open_browser
+  end
+
+  after :each do
+    close_browser
   end
 
   def get_element(selector)
@@ -19,8 +42,7 @@ describe 'app' do
     }
   end
 
-  context "firefox" do
-
+  context 'firefox' do
     before :all do
       `cd ../build && zip -r -FS /tmp/test.xpi * && cd -`
     end
@@ -29,12 +51,13 @@ describe 'app' do
       `rm -f /tmp/test.xpi`
     end
 
-    before :each do
+    def open_browser(language = 'en')
+      close_browser
       caps = Selenium::WebDriver::Remote::Capabilities.firefox(marionette: true)
       profile = Selenium::WebDriver::Firefox::Profile.new
       profile['ui.popup.disable_autohide'] = true
+      profile['intl.accept_languages'] = language
       @driver = Selenium::WebDriver.for(:firefox, desired_capabilities: caps, profile: profile)
-      
       bridge = @driver.send(:bridge)
       uri = URI(bridge.http.send(:server_url).to_s + 'session/' + bridge.session_id + '/moz/addon/install')
       http = Net::HTTP.new(uri.host, uri.port)
@@ -42,41 +65,43 @@ describe 'app' do
       req.body = { path: '/tmp/test.xpi', temporary: true }.to_json
       res = http.request(req)
       puts 'Tried to install extension, here is the response from geckodriver: ' + res.body
+      sleep 3
     end
 
-    after :each do
-      @driver.close
+    def close_browser
+      begin
+        @driver.close
+      rescue
+      end
     end
 
     def open_extension
-      @driver.navigate.to 'about:debugging'
-      get_element('#enable-addon-debugging').click
-      sleep 1
       @driver.action.key_down(:control).key_down(:shift).send_keys('l').key_up(:shift).key_up(:control).perform
       sleep 5
-      get_element('.debug-button').click
-      sleep 5
-      @driver.switch_to.alert.accept
-      sleep 5
-    end
 
-    it "should open extension" do
-      open_extension
+      # This is needed until geckodriver is able to switch to the extension's context automatically
+      @driver.navigate.to 'about:debugging'
+      sleep 3
+      id = get_element('.internal-uuid span')
+      @driver.navigate.to "moz-extension://#{id.text}/popup.html"
+      get_element('#app')
     end
-
-    it "should localize" do
-      # TODO
-    end
+    
+    include_examples 'tests'
   end
 
-  context "chrome" do
-    before :each do
-      @prefs = { 'intl.accept_languages': 'en' }
-      @driver = Selenium::WebDriver.for :chrome, switches: ['--load-extension=../build'], prefs: @prefs
+  context 'chrome' do
+    def open_browser(language = 'en')
+      close_browser
+      prefs = { 'intl.accept_languages' => language }
+      @driver = Selenium::WebDriver.for :chrome, switches: ['--load-extension=../build'], prefs: prefs
     end
 
-    after :each do
-      @driver.quit
+    def close_browser
+      begin
+        @driver.quit
+      rescue
+      end
     end
 
     def open_extension
@@ -87,20 +112,6 @@ describe 'app' do
       get_element('#app')
     end
 
-    it "should open extension" do
-      open_extension
-      message = get_element('#app h2')
-      expect(message.text.include?('Add to Check')).to be(true)
-    end
-
-    it "should localize" do
-      @prefs['intl.accept_languages'] = 'pt'
-      @driver = Selenium::WebDriver.for :chrome, switches: ['--load-extension=../build'], prefs: @prefs
-      open_extension
-      message = get_element('#app h2')
-      expect(message.text.include?('Adicionar ao Check')).to be(true)
-      @prefs['intl.accept_languages'] = 'en'
-      @driver = Selenium::WebDriver.for :chrome, switches: ['--load-extension=../build'], prefs: @prefs
-    end
+    include_examples 'tests'
   end
 end
