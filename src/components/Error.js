866ee5070169bcd834f68f54c5d3906f69525091
Caio Almeida <caiosba@gmail.com>
2017-10-02 19:52:31 -0300

    Ticket #6406: Fixing security issue

--- src/components/Error.js
+++ src/components/Error.js
@@ -10,7 +10,7 @@ class Error extends Component {
     return (
       <div id="error">
         <h2><FormattedMessage id="Error.thereWasAProblem" defaultMessage="Hmm, there was a problem" /></h2>
-        { this.props.message ? <code dangerouslySetInnerHTML={{ __html: message }}></code> : null }
+        { this.props.message ? <code>{message}</code> : null }
         { this.props.messageComponent ? <code>{this.props.messageComponent}</code> : null }
         <p>
           <FormattedMessage id="Error.tryAgain" defaultMessage="Please try again later." />
