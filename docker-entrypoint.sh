ba3c8d137357832bddc769400d4cb342b20f38d4
infojunkie <karim.ratib@gmail.com>
2020-08-25 12:44:03 -0700

    Cleanup build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,5 @@
 # browser extension (at ./build)
 npm run generate-extension
 
-# copy generated files
-mkdir -p dist && cp build.zip dist/
-
 # keep running, so we can run commands with `docker-compose exec`
 tail -f /dev/null
