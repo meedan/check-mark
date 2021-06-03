e37d4786b3a07af2117134ea83e7bb175f8912f5
Darius Kazemi <darius@meedan.com>
2021-06-03 16:07:15 -0700

    Location editing working
    * user can search for suggested locations using Mapbox suggestion API
    * user can customize place name
    * user can enter new lat/lng on text field and have it update the map
    * user can drag marker on map and it updates the lat/lng text field
    * selecting a suggested place name is "smart" and uses suggested boundary boxes to set zoom
    * unfortunately I could not use mapbox for the interactive map due to the fact that mozilla extentions do not honor content security policy settings for blob uris: https://bugzilla.mozilla.org/show_bug.cgi?id=1294996, instead I used Leaflet

---
+++ src/components/metadata/AutocompleteLocation.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import PropTypes from 'prop-types';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { TextField, CircularProgress } from '@material-ui/core';
+import config from './../../config';
+
+function AutocompleteLocation(props) {
+  const { setNameText, setCoordinates, map } = props;
+  const [open, setOpen] = React.useState(false);
+  const [options, setOptions] = React.useState([]);
+  const [searchText, setSearchText] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+
+  const fetchLocations = async (text) => {
+    const response = await fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${text}.json?access_token=${config.mapboxApiKey}&autocomplete=true`,
+    );
+    const data = await response.json();
+
+    setOptions(
+      data.features.map((item) => {
+        return {
+          name: item.place_name,
+          boundingBox: item.bbox,
+          center: item.center,
+        };
+      }),
+    );
+    setLoading(false);
+  };
+
+  React.useEffect(() => {
+    let timer;
+
+    if (searchText.length > 2) {
+      setLoading(true);
+      timer = setTimeout(fetchLocations, 1000, searchText);
+    }
+
+    return () => clearTimeout(timer);
+  }, [searchText]);
+
+  React.useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
+
+  return (
+    <Autocomplete
+      id="asynchronous-demo"
+      style={{ width: 300 }}
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      getOptionSelected={(option, value) => option.name === value.name}
+      getOptionLabel={(option) => option.name}
+      options={options}
+      filterOptions={(x) => x}
+      onInputChange={(e, newInputValue) => {
+        setSearchText(newInputValue);
+      }}
+      onChange={(e, value) => {
+        setNameText(value.name);
+        setCoordinates({
+          latitude: value.center[1],
+          longitude: value.center[0],
+          text: `${value.center[1]},${value.center[0]}`,
+          displayText: `${value.center[1]},${value.center[0]}`,
+          error: false,
+        });
+        if (value.boundingBox) {
+          map.fitBounds([
+            [value.boundingBox[1], value.boundingBox[0]],
+            [value.boundingBox[3], value.boundingBox[2]],
+          ]);
+        } else {
+          map.setZoom(map.defaultZoom);
+        }
+      }}
+      loading={loading}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={
+            <FormattedMessage
+              id="metadata.location.search"
+              defaultMessage="Search the map"
+              description="This is a label that appears on a text field. If the user begins to type a location they will receive a list of suggested place names."
+            />
+          }
+          variant="outlined"
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <React.Fragment>
+                {loading ? (
+                  <CircularProgress color="inherit" size={20} />
+                ) : null}
+                {params.InputProps.endAdornment}
+              </React.Fragment>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+}
+
+AutocompleteLocation.propTypes = {
+  setNameText: PropTypes.func.isRequired,
+  setCoordinates: PropTypes.func.isRequired,
+  map: PropTypes.object.isRequired,
+};
+
+export default AutocompleteLocation;
--- src/components/metadata/MetadataLocation.js
+++ src/components/metadata/MetadataLocation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 import { Typography, TextField } from '@material-ui/core';
 import L from 'leaflet';
@@ -7,6 +8,7 @@ import config from './../../config';
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
 import iconUrl from 'leaflet/dist/images/marker-icon.png';
 import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+import AutocompleteLocation from './AutocompleteLocation';
 
 // required for the Leaflet map pin icon images
 delete L.Icon.Default.prototype._getIconUrl;
@@ -30,40 +32,98 @@ function MetadataLocation({
 }) {
   const [firstMapRender, setFirstMapRender] = React.useState(true);
   const [map, setMap] = React.useState({});
-  let latitude, longitude;
+  const [marker, setMarker] = React.useState({});
+  let latitude, longitude, name;
   try {
-    [latitude, longitude] = JSON.parse(
+    const parsedNode = JSON.parse(
       JSON.parse(node.first_response?.content)[0].value,
-    ).geometry.coordinates;
+    );
+    [latitude, longitude] = parsedNode.geometry.coordinates;
+    name = parsedNode.properties.name;
   } catch {
     latitude = null;
     longitude = null;
   }
+  const [nameText, setNameText] = React.useState(name);
   const [coordinates, setCoordinates] = React.useState({
     latitude,
     longitude,
+    text: `${latitude},${longitude}`,
+    displayText: `${latitude},${longitude}`,
+    error: false,
   });
+
+  if (!coordinates.error && nameText) {
+    try {
+    setMetadataValue(
+      JSON.stringify({
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [coordinates.latitude, coordinates.longitude],
+        },
+        properties: {
+          name: nameText,
+        },
+      }),
+    );
+    } catch {
+      setMetadataValue('{}');
+    }
+  }
   const mutationPayload = {
     annotation_type: 'task_response_geolocation',
-    set_fields: `{"response_free_text":"${metadataValue}"}`,
+    set_fields: `{"response_geolocation":${JSON.stringify(metadataValue)}}`,
   };
 
   function handleLatLngChange(e) {
-    latitude = 46;
-    longitude = -68;
-    setCoordinates({ latitude, longitude });
-    setMetadataValue(e.target.value);
+    const parsedLatLng = e.target.value.match(
+      /^(-?\d+(\.\d+)?),\s*(-?\d+(\.\d+)?)$/,
+    );
+    if (parsedLatLng === null) {
+      setCoordinates({
+        latitude,
+        longitude,
+        text: '',
+        error: true,
+        displayText: e.target.value,
+      });
+    } else {
+      latitude = parsedLatLng[1];
+      longitude = parsedLatLng[3];
+      setCoordinates({
+        latitude,
+        longitude,
+        text: `${latitude},${longitude}`,
+        displayText: `${latitude},${longitude}`,
+        error: false,
+      });
+    }
   }
 
-  console.log('RENDERING', coordinates, map);
+  function handleNameChange(e) {
+    setNameText(e.target.value);
+  }
 
-  let defaultCenter, basemap, marker;
-  const defaultZoom = 10;
+  function handleMarkerMove(e) {
+    let latitude = e.target._latlng.lat;
+    let longitude = e.target._latlng.lng;
+    setCoordinates({
+      latitude,
+      longitude,
+      text: `${latitude},${longitude}`,
+      displayText: `${latitude},${longitude}`,
+      error: false,
+    });
+  }
 
   React.useEffect(() => {
     if (isEditing && firstMapRender) {
       setFirstMapRender(false);
       let tempMap = L.map('map-edit');
+      let defaultCenter, basemap;
+      const defaultZoom = 10;
+
       defaultCenter = [coordinates.latitude, coordinates.longitude];
       basemap = L.tileLayer(
         'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
@@ -72,15 +132,23 @@ function MetadataLocation({
             'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
         },
       );
-      marker = L.marker(defaultCenter);
+      let tempMarker = L.marker(defaultCenter, {
+        draggable: true,
+        autoPan: true,
+        autoPanPadding: L.point(20, 20),
+        autoPanSpeed: 10,
+      });
       tempMap.setView(defaultCenter, defaultZoom);
+      // overriding property on map object
+      tempMap.defaultZoom = defaultZoom;
       basemap.addTo(tempMap);
-      marker.addTo(tempMap);
+      tempMarker.addTo(tempMap);
+      tempMarker.on('moveend', handleMarkerMove);
       setMap(tempMap);
+      setMarker(tempMarker);
     } else if (isEditing && !firstMapRender) {
-      marker = L.marker([coordinates.latitude, coordinates.longitude]);
-      map.setView([coordinates.latitude, coordinates.longitude], defaultZoom);
-      marker.addTo(map);
+      marker.setLatLng([coordinates.latitude, coordinates.longitude]);
+      map.setView([coordinates.latitude, coordinates.longitude], map.getZoom());
     }
   });
 
@@ -94,7 +162,7 @@ function MetadataLocation({
             {node.first_response_value}
           </Typography>
           <img
-            src={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/${longitude},${latitude},10,0,0/400x400?access_token=pk.eyJ1IjoibWVlZGFuIiwiYSI6ImNqNmpsN3F5cDE3M3AyeW1xY2VpaHZybXEifQ.XpDu6rzlVTe0n0N7LAW2AA`}
+            src={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/${longitude},${latitude},10,0,0/400x400?access_token=${config.mapboxApiKey}`}
             alt=""
           />
           <img
@@ -117,27 +185,52 @@ function MetadataLocation({
         <>
           <Typography variant="h6">{node.label}</Typography>
           <Typography variant="body1">{node.description}</Typography>
+          <AutocompleteLocation {...{ setNameText, setCoordinates, map }} />
           <TextField
-            id="metadata-input"
-            label="Search the map"
+            label={
+              <FormattedMessage
+                id="metadata.location.customize"
+                defaultMessage="Customize place name"
+                description="This is a label that appears on a text field, related to a pin on a map. The user may type any text of their choice here and name the place they are pinning. They can also modify suggested place names here."
+              />
+            }
             variant="outlined"
+            value={nameText}
+            onChange={handleNameChange}
+            error={!nameText}
+            helperText={
+              !nameText ? 'Please enter a name for this location.' : null
+            }
           />
           <TextField
-            id="metadata-input"
-            label="Customize place name"
+            label={
+              <FormattedMessage
+                id="metadata.location.coordinates"
+                defaultMessage="Latitude, longitude"
+                description="This is a label that appears on a text field, related to a pin on a map. This contains the latitude and longitude coordinates of the map pin. If the user changes these numbers, the map pin moves. If the user moves the map pin, the numbers update to reflect the new pin location."
+              />
+            }
             variant="outlined"
-          />
-          <TextField
-            id="metadata-input"
-            label="Latitude, longitude"
-            variant="outlined"
-            value={`${latitude},${longitude}`}
+            value={coordinates.displayText}
             onChange={handleLatLngChange}
+            error={coordinates.error}
+            helperText={
+              coordinates.error ? (
+                <FormattedMessage
+                  id="metadata.location.coordinates.helper"
+                  defaultMessage="Should be a comma-separated pair of latitude and longitude coordiantes like '-12.9, -38.15'. Drag the map pin if you are having difficulty."
+                  description="This is a helper message that appears when someone enters text in the 'Latitude, longitude' text field that cannot be parsed as a valid pair of latitude and longitude coordinates. It tells the user that they need to provide valid coordinates and gives an example. It also tells them that they can do a drag action with the mouse on the visual map pin as an alternative to entering numbers in this field."
+                />
+              ) : null
+            }
           />
           <div id="map-edit" className={classes.map}></div>
           <br />
           <CancelButton />
-          <SaveButton {...{ mutationPayload }} />
+          <SaveButton
+            {...{ mutationPayload }}
+            disabled={coordinates.error || !nameText}
+          />
         </>
       )}
     </>
